4105fdc5e9c49336f441251ac42f65578d87bcee
Joshua Scott Brown <joshbro42867@yahoo.com>
2019-04-02 11:28:19 -0600

    Adding initial setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+
+from distutils.core import setup
+
+setup(
+    name='MD-Structure-Factor',
+    version='1.0',
+    packages=['md-structure-factor',],
+    license='',
+    log-description=open('README.txt').read(),
+        )
