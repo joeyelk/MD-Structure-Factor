34ee3ed7e90391543033c3fa9018b83d47e432ce
Joshua Scott Brown <joshbro42867@yahoo.com>
2019-04-02 17:37:37 -0600

    drop support for python 3.4

--- setup.py
+++ setup.py
@@ -7,5 +7,5 @@ setup(
     packages=['Md-Structure-Factor',],
     license=open('LICENSE').read(),
     long_description=open('README.md').read(),
-    install_requires=['wheel','setuptools','duecredit','tqdm','MDAnalysis','mdtraj','future','vtk','scipy','mayavi'],
+    install_requires=['wheel','setuptools','duecredit','tqdm','MDAnalysis','mdtraj','future','vtk','scipy','mayavi','numpy','pandas'],
         )
