a1cb4f9535d6ead5acc48a4499a4f8c43d0dd02b
Joshua Scott Brown <joshbro42867@yahoo.com>
2019-04-02 17:05:15 -0600

    Fixed tests to work no matter where it is run from

--- test/test_load_traj.py
+++ test/test_load_traj.py
@@ -1,16 +1,22 @@
 #! /usr/bin/env python
 
 import unittest
+import os
 import sys
-sys.path.insert(0,'../')
 
-" Import local files "
+path_to_tests=str(os.path.dirname(os.path.realpath(__file__)))
+path_to_tests+="/../"
+sys.path.insert(0,path_to_tests)
+
+# Import local files 
 import load_traj
 
 class TestLoadingTrajectoryFiles(unittest.TestCase):
 
     def test_load_gro(self):
-        atom_names = load_traj.load_gro("water.gro")
+        gro_file_path=str(os.path.dirname(os.path.realpath(__file__)))
+        gro_file_path+="/water.gro"
+        atom_names = load_traj.load_gro(gro_file_path)
         self.assertEqual(atom_names[0],"OW1")
         self.assertEqual(atom_names[1],"HW2")
         self.assertEqual(atom_names[2],"HW3")
